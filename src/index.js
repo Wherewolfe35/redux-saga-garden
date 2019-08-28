b8c7afa102e854e430f7c364dbf2c14518d445c9
Aaron Wolfe <3wolfeaaron5@gmail.com>
2019-08-28 15:35:56 -0500

    imports saga and sets up GET API request in PlantList

--- src/index.js
+++ src/index.js
@@ -1,28 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+//redux etc
+import createSagaMiddleWare from 'redux-saga';
+import {takeEvery, put} from 'redux-saga/effects';
+import logger from 'redux-logger';
+import axios from 'axios';
 
 import App from './App';
 
-// this startingPlantArray should eventually be removed
-const startingPlantArray = [
-  { id: 1, name: 'Rose' },
-  { id: 2, name: 'Tulip' },
-  { id: 3, name: 'Oak' }
-];
+//saga
+const sagaMiddleWare = createSagaMiddleWare();
 
-const plantList = (state = startingPlantArray, action) => {
+function* watcherSaga(){
+  yield takeEvery('OBTAIN_PLANTS', getSaga)
+}
+
+function* getSaga(){
+  try{
+    let plantResponse = yield axios.get('/api/plant');
+    console.log('Response from server', plantResponse.data);
+    yield put({
+      type: 'SET_PLANT_LIST',
+      payload: plantResponse.data
+    })
+} catch (error) {
+  console.log('error in GET', error);
+  
+}
+}
+
+//reducers
+const plantList = (state = [], action) => {
   switch (action.type) {
     case 'ADD_PLANT':
-      return [ ...state, action.payload ]
+      return [ ...state, action.payload ];
+    case 'SET_PLANT_LIST':
+      return action.payload;
     default:
       return state;
   }
 };
 
 const store = createStore(
-  combineReducers({ plantList }),
+  combineReducers({ 
+    plantList 
+  }),
+applyMiddleware(sagaMiddleWare, logger)
 );
+
+sagaMiddleWare.run(watcherSaga);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('react-root'));
